b519b1ae23662188b15ae8a0752185231730a299
grian32 <grianguy32@gmail.com>
2026-01-01 15:12:01 +0200

    feat: finish up emitting variable decl, ref, assign

--- emitter/emitter.go
+++ emitter/emitter.go
@@ -1,6 +1,8 @@
 package emitter
 
 import (
+	"fmt"
+	"grianlang3/lexer"
 	"grianlang3/parser"
 
 	"github.com/llir/llvm/ir"
@@ -10,12 +12,16 @@ import (
 )
 
 type Emitter struct {
-	m     *ir.Module
-	entry *ir.Block
+	m         *ir.Module
+	entry     *ir.Block
+	variables map[string]*ir.InstAlloca
+	varTypes  map[string]types.Type
 }
 
 func New() *Emitter {
 	e := &Emitter{m: ir.NewModule()}
+	e.variables = make(map[string]*ir.InstAlloca)
+	e.varTypes = make(map[string]types.Type)
 	e.m.NewFunc("dbg_i64", types.Void, ir.NewParam("val", types.I64))
 	main := e.m.NewFunc("main", types.I32)
 	entry := main.NewBlock("")
@@ -53,7 +59,44 @@ func (e *Emitter) Emit(node parser.Node) value.Value {
 		case "+":
 			return e.entry.NewAdd(left, right)
 		}
+	case *parser.DefStatement:
+		lt := varTypeToLlvm(node.Type)
+		right := e.Emit(node.Right)
+
+		vPtr := e.entry.NewAlloca(lt)
+		e.variables[node.Name] = vPtr
+		e.varTypes[node.Name] = lt
+		e.entry.NewStore(right, vPtr)
+		return right
+	case *parser.AssignmentStatement:
+		vPtr, ok := e.variables[node.Name]
+		if !ok {
+			fmt.Printf("compile error: couldn't find variable of name %s used in var assignment", node.Name)
+		}
+		right := e.Emit(node.Right)
+		e.entry.NewStore(right, vPtr)
+		return right
+	case *parser.RefExpression:
+		vPtr, ok := e.variables[node.Name]
+		if !ok {
+			fmt.Printf("compile error: couldn't find variable of name %s used in var ref", node.Name)
+		}
+		vType, ok := e.varTypes[node.Name]
+		if !ok {
+			fmt.Printf("compile error: couldn't find variable type of name %s used in var ref", node.Name)
+		}
+		return e.entry.NewLoad(vType, vPtr)
 	}
 
+	return nil
+}
+
+func varTypeToLlvm(vt lexer.VarType) types.Type {
+	switch vt {
+	case lexer.None:
+		return nil
+	case lexer.Int:
+		return types.I64
+	}
 	return nil
 }
