adc2c0d60cbdecb7cf0f56f3c53c772d7d8d403a
grian32 <grianguy32@gmail.com>
2026-02-20 23:25:32 +0200

    refactor: lexer now includes end line/col

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -90,71 +90,82 @@ func (l *Lexer) NextToken() Token {
 
 	switch l.ch {
 	case '-':
-		tok = l.doubleCharToken('>', MINUS, ARROW, l.currLine, l.currCh)
+		tok = l.doubleCharToken('>', MINUS, ARROW)
 	case '&':
-		tok = l.doubleCharToken('&', AMPERSAND, LAND, l.currLine, l.currCh)
+		tok = l.doubleCharToken('&', AMPERSAND, LAND)
 	case '|':
-		tok = l.doubleCharToken('|', UNKNOWN, LOR, l.currLine, l.currCh)
+		tok = l.doubleCharToken('|', UNKNOWN, LOR)
 	case '=':
-		tok = l.doubleCharToken('=', ASSIGN, EQ, l.currLine, l.currCh)
+		tok = l.doubleCharToken('=', ASSIGN, EQ)
 	case '!':
-		tok = l.doubleCharToken('=', NOT, NOTEQ, l.currLine, l.currCh)
+		tok = l.doubleCharToken('=', NOT, NOTEQ)
 	case '<':
-		tok = l.doubleCharToken('=', LT, LTEQ, l.currLine, l.currCh)
+		tok = l.doubleCharToken('=', LT, LTEQ)
 	case '>':
-		tok = l.doubleCharToken('=', GT, GTEQ, l.currLine, l.currCh)
+		tok = l.doubleCharToken('=', GT, GTEQ)
 	case 0:
 		tok.Literal = ""
 		tok.Type = EOF
-		tok.Position = PositionInfo{
-			Line: l.currLine,
-			Col:  l.currCh,
+		tok.Position = util.Position{
+			StartLine: l.currLine,
+			StartCol:  l.currCh,
+			EndLine:   l.currLine,
+			EndCol:    l.currCh,
 		}
 	default:
 		if l.ch == '0' && l.peekChar() == 'x' {
-			tok.Position = PositionInfo{
-				Line: l.currLine,
-				Col:  l.currCh,
+			tok.Position = util.Position{
+				StartLine: l.currLine,
+				StartCol:  l.currCh,
+				EndLine:   l.currLine,
 			}
 			tok.Literal = l.readHexaInt()
 			tok.Type = INT
+			tok.Position.EndCol = l.currCh
 			return tok
 		}
 
 		if util.IsDigit(l.ch) {
-			tok.Position = PositionInfo{
-				Line: l.currLine,
-				Col:  l.currCh,
+			tok.Position = util.Position{
+				StartLine: l.currLine,
+				StartCol:  l.currCh,
+				EndLine:   l.currLine,
 			}
 			tok.Literal, tok.Type = l.readNumber()
+			tok.Position.EndCol = l.currCh
 			return tok
 		}
 
 		if util.IsAlpha(l.ch) {
-			tok.Position = PositionInfo{
-				Line: l.currLine,
-				Col:  l.currCh,
+			tok.Position = util.Position{
+				StartLine: l.currLine,
+				StartCol:  l.currCh,
+				EndLine:   l.currLine,
 			}
 			l.readChar()
 			tok.Literal = l.readIdentifier()
 			tok.Type, tok.VarType.Base = identLookup(tok.Literal)
+			tok.Position.EndCol = l.currCh
 			return tok
 		}
 		if l.ch == '"' {
-			tok.Position = PositionInfo{
-				Line: l.currLine,
-				Col:  l.currCh,
+			tok.Position = util.Position{
+				StartLine: l.currLine,
+				StartCol:  l.currCh,
+				EndLine:   l.currLine,
 			}
 			l.readChar()
 			tok.Type = STRING
 			//tok.VarType = VarType{Base: Int8, Pointer: 1};
 			tok.Literal = l.readString()
+			tok.Position.EndCol = l.currCh
 		}
 
 		if l.ch == '\'' {
-			tok.Position = PositionInfo{
-				Line: l.currLine,
-				Col:  l.currCh,
+			tok.Position = util.Position{
+				StartLine: l.currLine,
+				StartCol:  l.currCh,
+				EndLine:   l.currLine,
 			}
 			l.readChar()
 			tok.Type = CHAR
@@ -164,6 +175,7 @@ func (l *Lexer) NextToken() Token {
 				tok.Literal = string(l.ch)
 				l.readChar() // skip next '
 			}
+			tok.Position.EndCol = l.currCh
 		}
 	}
 
@@ -218,26 +230,33 @@ func (l *Lexer) readIdentifier() string {
 }
 
 func newToken(tt TokenType, ch byte, currLine, currCh uint32) Token {
-	return Token{Type: tt, Literal: string(ch), Position: PositionInfo{Line: currLine, Col: currCh}}
+	return Token{Type: tt, Literal: string(ch), Position: util.Position{
+		StartLine: currLine,
+		EndLine:   currLine,
+		StartCol:  currCh,
+		EndCol:    currCh,
+	}}
 }
 
-func (l *Lexer) doubleCharToken(char2 byte, tt TokenType, tt2 TokenType, currLine, currCh uint32) Token {
+func (l *Lexer) doubleCharToken(char2 byte, tt TokenType, tt2 TokenType) Token {
 	var tok Token
 
 	if l.peekChar() == char2 {
 		l.readChar()
+		tok.Position = util.Position{
+			StartLine: l.currLine,
+			StartCol:  l.currCh,
+			EndCol:    l.currLine,
+		}
 		l.readChar()
 		tok.Type = tt2
 		tok.Literal = l.input[l.pos-2 : l.pos]
-		tok.Position = PositionInfo{
-			Line: currLine,
-			Col:  currCh,
-		}
+		tok.Position.EndCol = l.currCh
 		return tok
 	}
 
 	if tt != UNKNOWN {
-		tok = newToken(tt, l.ch, currLine, currCh)
+		tok = newToken(tt, l.ch, l.currLine, l.currCh)
 	}
 
 	return tok
