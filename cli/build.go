28c780227550a502d349c1dd4aed243a9a686497
grian32 <grianguy32@gmail.com>
2026-05-06 20:32:46 +0300

    feat: impl cli and move current global to build subcmd

---
+++ cli/build.go
@@ -0,0 +1,154 @@
+package cli
+
+import (
+	"embed"
+	"fmt"
+	"grianlang3/checker"
+	"grianlang3/emitter"
+	"grianlang3/lexer"
+	"grianlang3/parser"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+type BuildOpts struct {
+	KeepLL      bool
+	Dbg         bool
+	NoExecBuild bool
+}
+
+func RunBuildCmd(builtinFs embed.FS, files []string, opts *BuildOpts) error {
+	// TODO: really should do this in /tmp
+	if err := os.RemoveAll("./lltemp"); err != nil {
+		return err
+	}
+	if err := os.Mkdir("./lltemp", os.ModePerm); err != nil {
+		return err
+	}
+	var llFiles []string
+	builtinModules := map[string]struct{}{}
+	for _, file := range files {
+		input, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		l := lexer.New(string(input))
+		p := parser.New(l)
+		program := p.ParseProgram()
+		err = safeRun(func() {
+			if opts.Dbg {
+				log.Printf("%s: %s\n", file, program.String())
+			}
+		})
+		if err != nil {
+			return err
+		}
+		if len(p.Errors) != 0 {
+			for _, err := range p.Errors {
+				log.Printf("parser error: %s:%s\n", file, err.String())
+			}
+			return fmt.Errorf("%s: exiting after parser errrors\n", file)
+		}
+		c := checker.New()
+		c.Check(program)
+		if len(c.Errors) != 0 {
+			for _, err := range c.Errors {
+				log.Printf("checker warning: %s:%s\n", file, err.String())
+			}
+		}
+
+		e := emitter.New()
+		err = safeRun(func() {
+			e.Emit(program)
+		})
+		if len(e.Errors) != 0 {
+			for _, err := range e.Errors {
+				log.Printf("compiler error: %s:%s\n", file, err.String())
+			}
+			return fmt.Errorf("compiler errors\n")
+		}
+		if err != nil {
+			log.Printf("%s: recovered emitting llvm ir: %s\n", file, err)
+			return fmt.Errorf("compiler panic\n")
+		}
+		llvmIr := e.Module()
+
+		fileName := fmt.Sprintf("./lltemp/%s.ll", file)
+		llFile, err := os.Create(fileName)
+		if err != nil {
+			return fmt.Errorf("%s: %w\n", file, err)
+		}
+		_, err = fmt.Fprintf(llFile, "%s", llvmIr)
+		if err != nil {
+			return fmt.Errorf("%s: %w\n", file, err)
+		}
+		err = llFile.Close()
+		if err != nil {
+			return fmt.Errorf("%s: %w\n", file, err)
+		}
+
+		llFiles = append(llFiles, fileName)
+
+		for _, builtinModule := range e.BuiltinModules() {
+			builtinModules[builtinModule] = struct{}{}
+		}
+	}
+
+	for mod, _ := range builtinModules {
+		// kinda dodgy fix but realistically its going to be one or two modules that vendor c stuff directly so no point bothering
+		if mod == "ralloc.ll" {
+			continue
+		}
+		modText, err := builtinFs.ReadFile(fmt.Sprintf("builtins/%s", mod))
+		if err != nil {
+			return fmt.Errorf("failed to read %s from builtin fs: %w\n", mod, err)
+		}
+
+		fileName := fmt.Sprintf("./lltemp/%s", mod)
+		llFile, err := os.Create(fileName)
+		if err != nil {
+			return fmt.Errorf("failed to create %s: %w\n", llFile.Name(), err)
+		}
+		_, err = fmt.Fprintf(llFile, "%s", modText)
+		if err != nil {
+			return fmt.Errorf("failed to write %s: %w\n", llFile.Name(), err)
+		}
+		err = llFile.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close %s: %w\n", llFile.Name(), err)
+		}
+		llFiles = append(llFiles, fileName)
+	}
+
+	llFiles = append(llFiles, "-o", "out")
+	if !opts.NoExecBuild {
+		cmd := exec.Command("clang", llFiles...)
+		if opts.Dbg {
+			fmt.Printf("executing: %s\n", strings.Join(cmd.Args, " "))
+		}
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			return err
+		}
+	}
+	if !opts.KeepLL {
+		if err := os.RemoveAll("./lltemp"); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func safeRun(fn func()) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("panic: %v", r)
+		}
+	}()
+	fn()
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,10 +5,36 @@ go 1.25.5
 require github.com/llir/llvm v0.3.6
 
 require (
+	charm.land/lipgloss/v2 v2.0.0-beta.3.0.20251106193318-19329a3e8410 // indirect
+	github.com/charmbracelet/colorprofile v0.3.3 // indirect
+	github.com/charmbracelet/fang v1.0.0
+	github.com/charmbracelet/ultraviolet v0.0.0-20251106190538-99ea45596692 // indirect
+	github.com/charmbracelet/x/ansi v0.11.0 // indirect
+	github.com/charmbracelet/x/exp/charmtone v0.0.0-20250603201427-c31516f43444 // indirect
+	github.com/charmbracelet/x/term v0.2.2 // indirect
+	github.com/charmbracelet/x/termios v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.2.2 // indirect
+	github.com/clipperhouse/displaywidth v0.4.1 // indirect
+	github.com/clipperhouse/stringish v0.1.1 // indirect
+	github.com/clipperhouse/uax29/v2 v2.3.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.3.0 // indirect
+	github.com/mattn/go-runewidth v0.0.19 // indirect
 	github.com/mewmew/float v0.0.0-20201204173432-505706aa38fa // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/mango v0.1.0 // indirect
+	github.com/muesli/mango-cobra v1.2.0 // indirect
+	github.com/muesli/mango-pflag v0.1.0 // indirect
+	github.com/muesli/roff v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/tools v0.1.4 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/spf13/cobra v1.9.1
+	github.com/spf13/pflag v1.0.6 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
