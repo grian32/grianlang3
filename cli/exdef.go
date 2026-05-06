2c92166375d8959c7d079f76959efb1a998dff53
grian32 <grianguy32@gmail.com>
2026-05-07 01:27:49 +0300

    feat: strip casts/type infer in exdef

--- cli/exdef.go
+++ cli/exdef.go
@@ -2,7 +2,10 @@ package cli
 
 import (
 	"fmt"
+	"grianlang3/lexer"
+	"grianlang3/util"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -11,13 +14,15 @@ type ExDefOpts struct {
 	InFile  string
 }
 
+var castRegexp = regexp.MustCompile(`\([a-zA-Z_ ]+\)`)
+
 func RunExDef(opts *ExDefOpts) error {
 	out, err := exec.Command("clang", "-dM", "-E", opts.InFile).CombinedOutput()
 	if err != nil {
 		return err
 	}
 	defines := make(map[string]string)
-	for _, l := range strings.Split(string(out), "\n") {
+	for l := range strings.SplitSeq(string(out), "\n") {
 		split := strings.Split(l, " ")
 		if len(split) < 2 {
 			continue
@@ -26,7 +31,7 @@ func RunExDef(opts *ExDefOpts) error {
 		if strings.HasPrefix(name, "_") || strings.Contains(name, "(") {
 			continue
 		}
-		rest := strings.Trim(strings.Join(split[2:], " "), " ")
+		rest := strings.TrimSpace(strings.Join(split[2:], " "))
 		if rest == "" {
 			continue
 		}
@@ -35,7 +40,57 @@ func RunExDef(opts *ExDefOpts) error {
 	// TODO: strip casts, go off of value, float = float, integer = int64, strings=char* and char = char, special case void* 0 etc, output non regular expressions(A=B) as is will have to look up types though
 
 	for n, v := range defines {
-		fmt.Printf("%s = %s\n", n, v)
+		// NOTE: doing it this way rather than stripping at readtime because in the future id like to type based off of casts or alternatively special case for example void ptr cast to 0 as a nullptr
+		castStripped := stripCasts(v)
+		fmt.Printf("%s = %s of type %s\n", n, castStripped, inferType(castStripped).String())
 	}
 	return nil
+}
+
+func stripCasts(val string) string {
+	return strings.TrimSpace(
+		strings.TrimSuffix(
+			strings.TrimPrefix(
+				castRegexp.ReplaceAllString(val, ""),
+				"("),
+			")"),
+	)
+}
+
+func inferType(val string) lexer.VarType {
+	var currBaseType lexer.BaseVarType
+
+	if strings.Contains(val, "\"") {
+		return lexer.VarType{Base: lexer.Char, Pointer: 1}
+	}
+
+	if strings.Contains(val, "'") {
+		return lexer.VarType{Base: lexer.Char}
+	}
+
+	var hasDigit, hasOther, hasDot bool
+
+	for _, c := range val {
+		if util.IsDigit(byte(c)) {
+			hasDigit = true
+		} else if c == '.' {
+			hasDot = true
+		} else {
+			hasOther = true
+		}
+	}
+
+	if hasDigit {
+		currBaseType = lexer.Int
+	}
+
+	if hasDigit && hasDot {
+		currBaseType = lexer.Float
+	}
+
+	if hasOther {
+		currBaseType = lexer.None
+	}
+
+	return lexer.VarType{Base: currBaseType}
 }
